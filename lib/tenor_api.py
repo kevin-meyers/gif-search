ff85ee2035ef9013bbaf1444ef26c81e35e18ee0
Ivan Yerkinov <darkblackvoid@yahoo.com>
2019-09-17 13:39:36 -0700

    Added button to get trending gifs

--- lib/tenor_api.py
+++ lib/tenor_api.py
@@ -7,17 +7,31 @@ SEARCH_LIMIT = 10
 
 
 def search(query):
-	r = requests.get(
-		'https://api.tenor.com/v1/search?q=%s&key=%s&limit=%s' % (
-			query,
-			API_KEY,
-			SEARCH_LIMIT
-		)
-	)
-	if r.status_code == 200:
-		gifs_json = json.loads(r.content)
-		return gifs_json
-	
-	return None
+    r = requests.get(
+        'https://api.tenor.com/v1/search?q=%s&key=%s&limit=%s' % (
+            query,
+            API_KEY,
+            SEARCH_LIMIT
+        )
+    )
+    if r.status_code == 200:
+        gifs_json = json.loads(r.content)
+        return gifs_json
+
+    return None
 
 # TODO: Make trending request
+
+
+def searchTrending():
+    r = requests.get(
+        'https://api.tenor.com/v1/trending?key=%s&limit=%s' % (
+            API_KEY,
+            SEARCH_LIMIT
+        )
+    )
+
+    if r.status_code == 200:
+        gif_json = json.loads(r.content)
+        return gif_json
+    return None
